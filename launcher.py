97e433faacecc0d069d6962ac5acef01a2447e6f
ivarfol <varfolomeeviaroslav@gmail.com>
2024-11-24 14:59:56 +0000

    add docstrings

--- launcher.py
+++ launcher.py
@@ -3,6 +3,17 @@
 import termios, fcntl, sys, os, time, subprocess, pickle
 
 def key_detect():
+    '''
+    Detects a key press and turns off echo in the terminal while active
+
+    Parameters
+    ----------
+    None.
+
+    Returns
+    -------
+    None.
+    '''
     fd = sys.stdin.fileno()
 
     oldterm = termios.tcgetattr(fd)
@@ -26,6 +37,24 @@ def key_detect():
         fcntl.fcntl(fd, fcntl.F_SETFL, oldflags)
 
 def outp_table(new, shortcuts, pointer, old_pointer):
+    '''
+    Prints the table/menu
+
+    Parameters
+    ----------
+    new : 
+        print the entire board again
+    shortcuts :
+        list of lists with name and a list of the command and location to run
+        it at
+    pointer : int
+        position in the list
+    old_pointer : int
+        unused
+    Returns
+    -------
+    None.
+    '''
     print(f"\033[{len(shortcuts) + 1}F\033[0Jprogram name")
     if new:
         for i in range(len(shortcuts)):
@@ -38,6 +67,23 @@ def outp_table(new, shortcuts, pointer, old_pointer):
     return()
 
 def mov_pointer(pointer, limit, move):
+    '''
+    Manages the pointer position
+
+    Parameters
+    ----------
+    pointer : int
+        position in the list
+    limit : int
+        length of the list, prevent the pointer frpom going out of range
+    move : int
+        where to move the pointer
+
+    Returns
+    -------
+    pointer : int
+        position in the list
+    '''
     if pointer + move > limit:
         pointer = 0
     elif pointer + move < 0:
@@ -46,7 +92,18 @@ def mov_pointer(pointer, limit, move):
         pointer += move
     return(pointer)
 
-def helpf(limit):
+def helpf():
+    '''
+    Help function
+
+    Parameters
+    ----------
+    None.
+    
+    Returns
+    -------
+    None.
+    '''
     print(f"\033[H\033[0JHelp menu\nq to exit")
     print("w or k to go up, s or j to go down")
     print("r to remove a shortcut, n to make a new one")
@@ -57,14 +114,55 @@ def helpf(limit):
             break
 
 def new_shortcut(shortcuts):
+    '''
+    Appends a new shortcut
+    Parameters
+    ----------
+    shortcuts :
+        list of lists with name and a list of the command and location to run
+        it at
+
+    Returns
+    -------
+    shortcuts :
+        list of lists with name and a list of the command and location to run
+        it at
+    '''
     return(shortcuts + [[input("input the name:\n"), [input("what to run:\n"), input("where to run:\n")]]])
 
 def remove_shortcut(shortcuts, pointer):
+    '''
+    Removes a shortcut
+    Parameters
+    ----------
+    shortcuts :
+        list of lists with name and a list of the command and location to run
+        it at
+
+    Returns
+    -------
+    shortcuts :
+        list of lists with name and a list of the command and location to run
+        it at
+    '''
     shortcuts.pop(pointer)
     return(shortcuts)
 
 def main():
-    shortcuts = [["name", ["echo 'what did you expect'", "/"]]]
+    '''
+    Main function
+    calls other functions when a specific key is pressed and detected by
+    key_detect()
+
+    Parameters
+    ----------
+    None.
+    
+    Returns
+    -------
+    None.
+    '''
+    shortcuts = [["name", ["echo 'what did you expect'", "~"]]]
     if os.path.exists(os.path.join(os.path.abspath(os.path.dirname(__file__)), ".shortcuts.pb")):
         with open(".shortcuts.pb", "rb") as file:
             shortcuts = pickle.load(file)
